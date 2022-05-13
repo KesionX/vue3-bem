f08e174f6f082dae6b8ef2cf3fc64ed680583641
KesionX <apple@192.168.0.104>
2022-05-14 00:09:38 +0800

    fix: 插件问题

--- packages/vite-plugin-vue3-bem/src/index.ts
+++ packages/vite-plugin-vue3-bem/src/index.ts
@@ -1,43 +1,45 @@
 import { parse, compileScript } from "@vue/compiler-sfc";
 import MagicString from "magic-string";
 
-function supportScriptName(code: string, id: string) {
+function supportScriptName(code: string, id: string, optionLang?: "ts" | "js") {
   let s: MagicString;
   const str = () => s || (s = new MagicString(code));
   const { descriptor } = parse(code);
-  if (!descriptor.script && descriptor.scriptSetup) {
+  let name = "block";
+  let lang: any = optionLang || "js";
+  if (descriptor.scriptSetup) {
     const result = compileScript(descriptor, { id });
-    const name = result.attrs["bem-block"];
-    const lang = result.attrs.lang;
-    if (name) {
-      str().appendLeft(
-        0,
-        `<script ${lang ? `lang="${lang}"` : ""}>
+    name = (result.attrs["bem-block"] as string | undefined) || "block";
+    lang = (result.attrs["lang"] as string | undefined) || optionLang;
+  }
+
+  str().appendLeft(
+    0,
+    `<script ${lang ? `lang="${lang}"` : ""}  ${
+      !descriptor.scriptSetup ? "setup" : ""
+    }>
 import useBem from "vue3-bem";
 const bem = useBem("${name}");
 <\/script>
 `
-      );
-    }
-    return {
-      map: str().generateMap(),
-      code: str().toString(),
-    };
-  } else {
-    return null;
-  }
+  );
+  return {
+    map: str().generateMap(),
+    code: str().toString(),
+  };
 }
 
 const fileRegex = /\.(vue)$/;
 
-function ViteVue3BemPlugin() {
+function ViteVue3BemPlugin(option?: { lang?: "ts" | "js" }) {
   return {
     name: "vite-plugin-vue3-bem",
     enforce: "pre",
     transform(code: string, id: string) {
       if (fileRegex.test(id)) {
-        return supportScriptName.call(this, code, id);
+        return supportScriptName.call(this, code, id, option?.lang);
       }
+      return null;
     },
   };
 }
