8d61c746274eefa338a95fd4bbdad0f24b53a4e7
柯建芳 <673080885@qq.com>
2022-05-19 23:32:01 +0800

    fix: 只对setup定义了block生效

--- packages/vite-plugin-vue3-bem/src/index.ts
+++ packages/vite-plugin-vue3-bem/src/index.ts
@@ -9,15 +9,18 @@ function supportScriptName(code: string, _id: string) {
   });
   // 只在script setup写法内作用
   if (descriptor.scriptSetup && descriptor.scriptSetup.setup) {
-    const name = descriptor.scriptSetup.attrs?.["bem-block"] || "block";
-    const offset = descriptor.scriptSetup.loc.start.offset;
-    str().appendLeft(
-      offset,
-      `
-    import useBem from "vue3-bem";
-    const bem = useBem("${name}");
-    `
-    );
+    const attrs = descriptor.scriptSetup.attrs;
+    const name = attrs?.["bem-block"] || attrs["b"];
+    if (name) {
+        const offset = descriptor.scriptSetup.loc.start.offset;
+        str().appendLeft(
+          offset,
+          `
+        import useBem from "vue3-bem";
+        const bem = useBem("${name}");
+        `
+        );
+    }
   }
   return {
     map: str().generateMap(),
